59049cc7a58c33173df8c0f1d7f4684bfc00ceb3
Andy <aw087ok@gmail.com>
2019-09-20 10:35:41 +0100

    Activity file now uses Placemark data class to store title

--- app/src/main/java/com/example/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/com/example/placemark/activities/PlacemarkActivity.kt
@@ -2,6 +2,7 @@ package com.example.placemark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.models.PlacemarkModel
 import com.example.placemark.R
 import kotlinx.android.synthetic.main.activity_placemark.*
 import org.jetbrains.anko.AnkoLogger
@@ -10,15 +11,17 @@ import org.jetbrains.anko.toast
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
+    var placemark = PlacemarkModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark)
         info("Placemark Activity started..")
 
         btnAdd.setOnClickListener() {
-            val placemarkTitle = placemarkTitle.text.toString()
-            if (placemarkTitle.isNotEmpty()) {
-                info("Add button pressed: $placemarkTitle")
+            placemark.title = placemarkTitle.text.toString()
+            if (placemark.title.isNotEmpty()) {
+                info("Add button pressed: $placemark")
             } else {
                 toast("Please enter a title..")
             }
