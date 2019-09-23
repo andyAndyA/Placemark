ba97cecda3f3e3687767fb2f70d620508ca0013a
Andy <aw087ok@gmail.com>
2019-09-23 16:02:22 +0100

    Moved PlacemarkAdapter functionality to its own source file

--- app/src/main/java/com/example/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/com/example/placemark/activities/PlacemarkListActivity.kt
@@ -4,14 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.placemark.main.MainApp
 import com.example.placemark.R
-import com.example.placemark.models.PlacemarkModel
-import kotlinx.android.synthetic.main.activity_placemark.view.*
+import com.example.placemark.models.PlacemarkAdapter
 import kotlinx.android.synthetic.main.activity_placemark_list.*
-import kotlinx.android.synthetic.main.card_placemark.view.*
-import kotlinx.android.synthetic.main.card_placemark.view.placemarkTitle
 import org.jetbrains.anko.startActivityForResult
 
 class PlacemarkListActivity : AppCompatActivity() {
@@ -41,34 +37,5 @@ class PlacemarkListActivity : AppCompatActivity() {
             R.id.item_add -> startActivityForResult<PlacemarkActivity>(0)
         }
         return super.onOptionsItemSelected(item)
-    }
-}
-
-class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>) :
-    RecyclerView.Adapter<PlacemarkAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        return MainHolder(
-            LayoutInflater.from(parent?.context).inflate(
-                R.layout.card_placemark,
-                parent,
-                false
-            )
-        )
-    }
-
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val placemark = placemarks[holder.adapterPosition]
-        holder.bind(placemark)
-    }
-
-    override fun getItemCount(): Int = placemarks.size
-
-    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        fun bind(placemark: PlacemarkModel) {
-            itemView.placemarkTitle.text = placemark.title
-            itemView.description.text = placemark.description
-        }
     }
 }
