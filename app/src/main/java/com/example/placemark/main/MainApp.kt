12325155a52bb1706880f8e4416cf0e36aa13fc6
Andy <aw087ok@gmail.com>
2019-09-23 11:35:35 +0100

    Added main directory

---
+++ app/src/main/java/com/example/placemark/main/MainApp.kt
@@ -0,0 +1,13 @@
+package com.example.placemark.main
+
+import android.app.Application
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+class MainApp: Application(), AnkoLogger {
+
+    override fun onCreate() {
+        super.onCreate()
+        info("Placemark started")
+    }
+}
