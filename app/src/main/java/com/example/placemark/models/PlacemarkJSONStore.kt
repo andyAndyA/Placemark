624c89a4da6fc0886fbb881f2a80beffc8473e53
Andy <aw087ok@gmail.com>
2019-10-22 00:10:48 +0100

    New model file for storing and retrieving placemarks via JSON file serialization

---
+++ app/src/main/java/com/example/placemark/models/PlacemarkJSONStore.kt
@@ -0,0 +1,54 @@
+package com.example.placemark.models
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import org.jetbrains.anko.AnkoLogger
+import com.example.placemark.helpers.*
+import java.util.*
+
+val JSON_FILE = "placemarks.json"
+val gsonBuilder = GsonBuilder().setPrettyPrinting().create()
+val listType = object : TypeToken<java.util.ArrayList<PlacemarkModel>>() {}.type
+
+fun generateRandomId(): Long {
+    return Random().nextLong()
+}
+
+class PlacemarkJSONStore: PlacemarkStore, AnkoLogger {
+
+    val context: Context
+    var placemarks = mutableListOf<PlacemarkModel>()
+
+    constructor(context: Context) {
+        this.context = context
+        if (exists(context, JSON_FILE)) {
+            deserialize()
+        }
+    }
+
+    override fun findAll(): MutableList<PlacemarkModel> {
+        return placemarks
+    }
+
+    override fun create(placemark: PlacemarkModel) {
+        placemark.id = generateRandomId()
+        placemarks.add(placemark)
+        serialize()
+    }
+
+    override fun update(placemark: PlacemarkModel) {
+        // TODO
+    }
+
+    private fun serialize() {
+        val jsonString = gsonBuilder.toJson(placemarks, listType)
+        write(context, JSON_FILE, jsonString)
+    }
+
+    private fun deserialize() {
+        val jsonString = read(context, JSON_FILE)
+        placemarks = Gson().fromJson(jsonString, listType)
+    }
+}
