4c88ce666cf4ba4f40a30ead53cfdf8b94ffd1f0
Andy <aw087ok@gmail.com>
2019-10-22 00:14:20 +0100

    Now using new placemarks JSON serialization model

--- app/src/main/java/com/example/placemark/main/MainApp.kt
+++ app/src/main/java/com/example/placemark/main/MainApp.kt
@@ -1,21 +1,21 @@
 package com.example.placemark.main
 
 import android.app.Application
+import com.example.placemark.models.PlacemarkJSONStore
 import com.example.placemark.models.PlacemarkMemStore
 import com.example.placemark.models.PlacemarkModel
+import com.example.placemark.models.PlacemarkStore
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
 class MainApp: Application(), AnkoLogger {
 
-    val placemarks = PlacemarkMemStore()
+    lateinit var placemarks: PlacemarkStore
 
     override fun onCreate() {
         super.onCreate()
-        info("Placemark started")
 
-//        placemarks.add(PlacemarkModel("One", "About one..."))
-//        placemarks.add(PlacemarkModel("Two", "About two..."))
-//        placemarks.add(PlacemarkModel("Three", "About three..."))
+        placemarks = PlacemarkJSONStore(applicationContext)
+        info("Placemark started")
     }
 }
