3a3a8e80e7485b3c8218bcd1dfb89b95b63fa0a6
Andy <aw087ok@gmail.com>
2019-11-04 11:58:52 +0000

    Lifecycle methods for displaying map added to activity

--- app/src/main/java/com/example/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/com/example/placemark/activities/PlacemarkMapsActivity.kt
@@ -1,14 +1,18 @@
 package com.example.placemark.activities
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.placemark.R
+import com.google.android.gms.maps.GoogleMap
 
 import kotlinx.android.synthetic.main.activity_placemark_maps.*
 import kotlinx.android.synthetic.main.content_placemark_maps.*
 
 class PlacemarkMapsActivity : AppCompatActivity() {
+
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +20,30 @@ class PlacemarkMapsActivity : AppCompatActivity() {
         toolbar.title = title
         setSupportActionBar(toolbar)
         mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
