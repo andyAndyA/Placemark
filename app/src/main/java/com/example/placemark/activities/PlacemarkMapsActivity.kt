c9c834623556785e7d8711c3ce9a9fa77425941f
Andy <aw087ok@gmail.com>
2019-11-04 11:54:03 +0000

    Refactored generated code from basic activity

--- app/src/main/java/com/example/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/com/example/placemark/activities/PlacemarkMapsActivity.kt
@@ -12,12 +12,7 @@ class PlacemarkMapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark_maps)
+        toolbar.title = title
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
-
 }
